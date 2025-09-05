c8ed307c9600fdfb2d5cc7fcc25d0d65d9a47bab
Pierre Chapuis <git@catwell.info>
2025-09-05 11:55:10 +0200

    add useful typings

---
+++ typings/safetensors/torch.pyi
@@ -0,0 +1,10 @@
+import os
+
+import torch
+
+def save_file(
+    tensors: dict[str, torch.Tensor],
+    filename: str | os.PathLike[str],
+    metadata: dict[str, str] | None = None,
+) -> None: ...
+def load_file(filename: str | os.PathLike[str]) -> dict[str, torch.Tensor]: ...
