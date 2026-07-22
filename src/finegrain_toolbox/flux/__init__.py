219fee8876edcad97bda8a8b2f32327609f40d59
Pierre Chapuis <git@catwell.info>
2026-07-22 14:51:18 +0200

    remove support for Flux
    (it will be kept in branch legacy/flux)

--- src/finegrain_toolbox/flux/__init__.py
+++
@@ -1,6 +0,0 @@
-from .latents import LatentReshaper
-from .model import Model
-from .positional_embedding import get_kontext_image_ids
-from .prompt import Prompt, TextEncoder
-
-__all__ = ["LatentReshaper", "Model", "Prompt", "TextEncoder", "get_kontext_image_ids"]
